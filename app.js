c44988d75fcba9fc6121e56f138ecf8c08bc8d12
ahmedraza95 <ahmedhome6789@gmail.com>
2024-05-27 23:52:54 +0500

    secoo

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ form.addEventListener("submit", async (event) => {
 
     try {
 
+
+
+
         let currDate = document.querySelector("#dateTime");
         let userCity = document.querySelector("#Email").value;
         let form = document.querySelector("#form")
@@ -27,69 +30,71 @@ form.addEventListener("submit", async (event) => {
         let div4value = document.querySelector("#div4value")
         let div5value = document.querySelector("#div5value")
         // let div6value = document.querySelector("#div6value")
-        let divtemp5 = document.querySelector(".weather")
-        
-        
-        
+        let divtemp5 = document.querySelector(".weather");
+        location.style.display = "inline"
+
+
         ApiKey = `https://api.weatherapi.com/v1/current.json?key=6b98efe0404240d2b8c175044241905&q=${userCity}&aqi=no;`
         const url = await fetch(ApiKey);
-    let response = await url.json();
-    location.innerHTML = `Weather Today In ${response.location.name}, ${response.location.country}`;
-    let correct = response.current.condition.icon;
-    
-    correct = correct.toString();
-    correct = correct.split("/");
-    correct = correct.splice(3)
-    correct = "./" + correct[0] + "/" + correct[1] + "/" + correct[2] + "/" + correct[3]
-    
-    let weatherimg = `<img src="${correct}">`
-    divtemp5.innerHTML = `Weather ${response.current.condition.text}${weatherimg}`
-    
-    
-    divtemp1.innerHTML = `<img src="./sun logo.png" alt=""> Temp`
-    div1value.innerHTML = `${response.current.temp_c}&#176 C`
-    
-    divtemp2.innerHTML = `<img src="./imsges/wind icon.png" width="10%" alt=""> Wind `
-    div2value.innerHTML = `${response.current.wind_kph}km/h`
-    
-    divtemp3.innerHTML = `<img src="./imsges/humidity.png" width="10%" alt=""> Humidity`
-    div3value.innerHTML = `${response.current.humidity}%`
-    
-    divtemp4.innerHTML = `<img src="./imsges/visibility.png" width="10%" alt=""> visibility`
-    div4value.innerHTML = `${response.current.vis_km} km`
-    
-    div5temp.innerHTML = `<img src="./imsges/pressuer.png" width="10%" alt=""> Pressure`
-    div5value.innerHTML = `<img src="./imsges/right-arrow.png" alt=""> ${response.current.pressure_mb.toFixed(1)} mb`
-    
-    
-} catch (error) {
-    // form.addEventListener("submit", async (event) => {
-    console.log(sweetAlert("City Is not Found", "Please Input Correct City!", "error"));
+        let response = await url.json();
+        location.innerHTML = `Weather Today In ${response.location.name}, ${response.location.country}`;
+        let correct = response.current.condition.icon;
 
-}
-    
-    
-    
+        correct = correct.toString();
+        correct = correct.split("/");
+        correct = correct.splice(3)
+        correct = "./" + correct[0] + "/" + correct[1] + "/" + correct[2] + "/" + correct[3]
+
+        let weatherimg = `<img src="${correct}">`
+        divtemp5.innerHTML = `Weather ${response.current.condition.text}${weatherimg}`
+
+        divtemp1.innerHTML = `<img src="./sun logo.png" alt=""> Temp`
+        div1value.innerHTML = `${response.current.temp_c}&#176 C`
+
+        divtemp2.innerHTML = `<img src="./imsges/wind icon.png" width="10%" alt=""> Wind `
+        div2value.innerHTML = `${response.current.wind_kph}km/h`
+
+        divtemp3.innerHTML = `<img src="./imsges/humidity.png" width="10%" alt=""> Humidity`
+        div3value.innerHTML = `${response.current.humidity}%`
+
+        divtemp4.innerHTML = `<img src="./imsges/visibility.png" width="10%" alt=""> visibility`
+        div4value.innerHTML = `${response.current.vis_km} km`
+
+        div5temp.innerHTML = `<img src="./imsges/pressuer.png" width="10%" alt=""> Pressure`
+        div5value.innerHTML = `<img src="./imsges/right-arrow.png" alt=""> ${response.current.pressure_mb.toFixed(1)} mb`
+        // div1value = "Hello";
+
+    } catch (error) {
+
+        location.innerHTML = `<img src="./imsges/pressuer.png" sorry`;
+        console.log(error);
+        // form.addEventListener("submit", async (event) => {
+        console.log(sweetAlert("City Is not Found", "Please Input Correct City!", "error"));
+
+    }
+
+
+
     // "//cdn.weatherapi.com
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
 
 
