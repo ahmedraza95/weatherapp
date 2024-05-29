c32ea0a10cad15a7f85bddd4b4b0b4b020638e3c
ahmedraza95 <ahmedhome6789@gmail.com>
2024-05-29 19:52:19 +0500

    10th

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ setInterval(() => {
 
 let card = document.querySelector("#card");
 let location = document.querySelector(".location");
+let loader = document.querySelector("#wifi-loader");
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -64,6 +65,24 @@ form.addEventListener("submit", async (event) => {
         div4value.innerHTML = "";
         div5value.innerHTML = "";
         div5temp.innerHTML = "";
+        location.innerHTML = `           
+        <div id="wifi-loader">
+            <svg class="circle-outer" viewBox="0 0 86 86">
+                <circle class="back" cx="43" cy="43" r="40"></circle>
+                <circle class="front" cx="43" cy="43" r="40"></circle>
+                <circle class="new" cx="43" cy="43" r="40"></circle>
+            </svg>
+        <svg class="circle-middle" viewBox="0 0 60 60">
+            <circle class="back" cx="30" cy="30" r="27"></circle>
+            <circle class="front" cx="30" cy="30" r="27"></circle>
+        </svg>
+        <svg class="circle-inner" viewBox="0 0 34 34">
+            <circle class="back" cx="17" cy="17" r="14"></circle>
+            <circle class="front" cx="17" cy="17" r="14"></circle>
+        </svg>
+        <div class="text" data-text="Searching"></div>
+    </div>
+    </div>`;
 
 
 
